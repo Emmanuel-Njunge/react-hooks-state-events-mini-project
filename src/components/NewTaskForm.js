4ef7ad297d7a414c1b95eef4213f90d14f01d266
Emmanuel Njungé Ngugi <ngugiemmanuelnjunge@gmail.com>
2025-04-30 08:27:33 +0300

    Implemented Task List App with state and events

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [taskText, setTaskText] = useState("");
+  const [taskCategory, setTaskCategory] = useState(categories[1]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ id: uuid(), text: taskText, category: taskCategory });
+    setTaskText("");
+    setTaskCategory(categories[1]);
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={taskText} onChange={(e) => setTaskText(e.target.value)} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={taskCategory} onChange={(e) => setTaskCategory(e.target.value)}>
+          {categories.filter((category) => category !== "All").map((category) => (
+            <option key={category} value={category}>{category}</option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
